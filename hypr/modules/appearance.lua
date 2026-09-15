6e6924e49eea2fd21b789d0cbcc66d8f57374371
tfqhkl <taufiqhaikal1808@gmail.com>
2026-09-15 22:20:41 +0800

    edit gaps

--- hypr/modules/appearance.lua
+++ hypr/modules/appearance.lua
@@ -1,8 +1,8 @@
 -- appearance.
   hl.config({
     general = {
-      gaps_in  = 10,
-      gaps_out = 20,
+      gaps_in  = 4, -- 10 for wide
+      gaps_out = 10, -- 20 for wide
       resize_on_border = false,
       allow_tearing = false,
       layout = master,
